c3f61a35fda5e80b28ef3ae00de2e0472122f3cb
timoxa2t <alex2t172@gmail.com>
2022-08-20 11:49:47 +0300

    FIX

--- src/index.js
+++ src/index.js
@@ -7,26 +7,50 @@ const searchEl = document.querySelector(`input[name="searchQuery"]`)
 const searchBtn = document.querySelector(`button[type="submit"]`)
 const loadMoreBtn = document.querySelector(`.load-more`)
 loadMoreBtn.toggleAttribute("hidden")
-let mPage = 1
+
+let searchText = ""
+
+const baseURL = "https://pixabay.com/api/?"
+
+const queryParams = new URLSearchParams({
+    key: "29290264-6b96edcd60ed9fd64e47be52c",
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true, 
+    per_page: 40, 
+    page: 1
+})
 
 searchBtn.addEventListener("click", (event) => {
     event.preventDefault()
     containerEl.textContent = ""
-    mPage = 1
-    updateView()
-    loadMoreBtn.removeAttribute("hidden")
+    searchText = searchEl.value.trim()
+    if(searchText){
+        queryParams.set("page", 1)
+        updateView()
+    }else{
+        loadMoreBtn.setAttribute("hidden", true)
+        Notiflix.Notify.warning("Search query is empty");
+    }
 })
 
 loadMoreBtn.addEventListener("click", (event) => {
+    nextPage()
     updateView()
 })
 
 async function updateView(){
-    const searchText = searchEl.value
-    const data = await sendRequest(searchText, mPage++)
-    console.log(data)
+    const data = await sendRequest(searchText)
+
     if(data.hits && data.hits.length > 0){
+        searchEl.value = ""
         createCards(data.hits)
+        if(data.totalHits <= queryParams.get("per_page") * queryParams.get("page")){
+            loadMoreBtn.setAttribute("hidden", true)
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        }else{
+            loadMoreBtn.removeAttribute("hidden")
+        }
     }else{
         loadMoreBtn.setAttribute("hidden", true)
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
@@ -61,20 +85,17 @@ function createCards(imgData){
 }
 
 function sendRequest(queryText, page){
-    
-   const params = new URLSearchParams({
-        key: "29290264-6b96edcd60ed9fd64e47be52c",
-        q: queryText, 
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true, 
-        per_page: 20, 
-        page: page
-   })
 
-   const url = "https://pixabay.com/api/?" + params
+    queryParams.set('q', queryText)
+    console.log(queryParams.toString())
 
-   return axios.get(url)
-    .then((res) => res.data)
-    .catch(err => err)
+    const url = baseURL + queryParams
+
+    return axios.get(url)
+        .then((res) => res.data)
+        .catch(err => err)
+}
+
+function nextPage(){
+    queryParams.set('page', parseInt(queryParams.get("page")) + 1)
 }
